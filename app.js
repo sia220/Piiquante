47b982bea52d7fa2808f2f582ff47854d7447af0
Sadia <Esadia@hotmail.fr>
2024-03-12 16:24:44 +0000

    ajout de deux gestionnaires de routage (pour chemin api/sauces et chemin images)

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
+const path = require('path');
+
+// On importe les routeurs
+const saucesRoutes = require('./routes/sauces') 
 const userRoutes = require('./routes/user');
 
+// Connexion à mongoDB (la base de données)
 mongoose.connect('mongodb+srv://user:user@cluster0.kdwhose.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
 {   useNewUrlParser : true,
     useUnifiedTopology : true})
@@ -20,7 +25,10 @@ app.use((req, res, next) => {
 // Ce middleware intercepte les requêtes ayant comme Content-Type application/json et met à disposition leur body directement sur l'objet req
 app.use(express.json())
 
+// Nos routes
+app.use('/api/sauces', saucesRoutes)
 app.use('/api/auth', userRoutes);
+app.use('/images', express.static(path.join(__dirname, 'images')));
 
 module.exports = app
 
