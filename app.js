9d43a77aba319a8a95804e5ad751ff26f0b59ae7
Sadia <Esadia@hotmail.fr>
2024-03-12 15:40:17 +0000

    mise en place application

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+const userRoutes = require('./routes/user');
+
+mongoose.connect('mongodb+srv://user:user@cluster0.kdwhose.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
+{   useNewUrlParser : true,
+    useUnifiedTopology : true})
+    .then(() => console.log('Connexion à MongoDB réussie'))
+    .catch(() => console.log('Connexion à MongoDB échouée'))
+  
+// J'autorise les requêtes entre le back et le front    
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
+
+// Ce middleware intercepte les requêtes ayant comme Content-Type application/json et met à disposition leur body directement sur l'objet req
+app.use(express.json())
+
+app.use('/api/auth', userRoutes);
+
+module.exports = app
+
+
+
+
+
