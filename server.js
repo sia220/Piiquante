e9ea094c479bc22d9c4eba87e9c2c54f55705e0f
Sadia <Esadia@hotmail.fr>
2024-03-12 15:39:53 +0000

    mise en place serveur

---
+++ server.js
@@ -0,0 +1,52 @@
+const http = require('http');
+const app = require('./app');
+
+// Fonction qui renvoie un port valide
+const normalizePort = val => {
+  const port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    return val;
+  }
+  if (port >= 0) {
+    return port;
+  }
+  return false;
+};
+const port = normalizePort(process.env.PORT || '3000');
+// On indique à l'appli sur quel port elle va tourner
+app.set('port', port);
+
+// Fonction qui gère les erreurs
+const errorHandler = error => {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+  const address = server.address();
+  const bind = typeof address === 'string' ? 'pipe ' + address : 'port: ' + port;
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges.');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use.');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+};
+
+// On exécute l'application sur le serveur . Les requêtes clients sont maintenant transférées à l'appli.
+const server = http.createServer(app);
+
+server.on('error', errorHandler);
+server.on('listening', () => {
+  const address = server.address();
+  const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
+  console.log('Listening on ' + bind);
+});
+
+// On indique au serveur le port qu'il doit écouter
+server.listen(port);
